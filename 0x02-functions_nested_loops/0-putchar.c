5231cc046a1c9e31678969319a0ef45fd7a41df1
root <kodongochieng@gmail.com>
2022-03-16 14:50:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints to console
+ *
+ * Description: prints _putchar string to console
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar"
+	int i = 0;
+
+	while (text[i] != 0)
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}	
