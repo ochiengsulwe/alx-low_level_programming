050adb8220201893f5739c368f422d7e121178cf
ochiengsulwe <kodongochieng@gail.com>
2022-07-05 23:04:34 -0700

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
- * jack_bauer - prints time table in this format: HH:MM
- * Return: the sum of two numbers.
+ * print_last_digit - prints the last digit of a numer.
+ * @n: digit to find the last place of.
+ * Return: The last digit.
  */
-void jack_bauer(void)
+int print_last_digit(int n)
 {
-	int h, m;
+	int a;
 
-	for (h = 0; h < 24; h++)
-	{
-		for (m = 0; m < 60; m++)
-		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '10');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
-		}
-	}
+	if (n < 0)
+	{n = -n; }
+
+	a = n % 10;
+
+	if (a < 0)
+	{a = -a; }
+
+	_putchar(a + 'a');
+
+	return (a);
 }
-
