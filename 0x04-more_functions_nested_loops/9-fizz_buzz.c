c3e212dc0c0f42ebe2df7ee38ed9ffc5d1947875
ochiengsulwe <kodongochieng@gail.com>
2022-07-08 13:52:08 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % != 5)
+		if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if (i % 5 == 0 && i % != 3)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
@@ -33,6 +33,12 @@ int main(void)
 		{
 			printf(" %d", i);
 		}
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
 	}
 	printf("\n");
 
