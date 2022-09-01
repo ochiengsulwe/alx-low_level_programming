5187410b4b6254f8eca3fc0e8a0170805bbb911c
ochiengsulwe <kodongochieng@gail.com>
2022-09-01 06:41:46 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+  * struct dog - a new type dog
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  * Description: nothing much to say about it ye
+  */
+
+struct dog
+{
+	char *name,*owner;
+	float age;
+};
