90bb0580788fc7d712474a58b64e139cb55b8bf2
ochiengsulwe <kodongochieng@gmail.com>
2023-03-22 11:05:37 -0700

    change: reduce line 9 to less than 80 characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	fputs("and that piece of
-			art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	fputs("\n", stderr);
 
 	return (1);
 }
