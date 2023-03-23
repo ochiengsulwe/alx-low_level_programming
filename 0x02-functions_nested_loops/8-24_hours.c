89ac79bb5a12fbf7b84ccba3527245a6460b4bf2
ochiengsulwe <kodongochieng@gmail.com>
2023-03-23 03:21:57 -0700

    add: function terminating loop at 2359

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,23 +8,20 @@ void jack_bauer(void)
 	int a, b, c, d;
 
 	for (a = '0'; a <= '2'; a++)
-	{
+	{		
 		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c <= '5'; c++)
+			if ((a <= '1' && b <= '9') || (a <= '2' && b <= '3'))
 			{
-				for (d = '0'; d <= '9'; d++)
+				for (c = '0'; c <= '5'; c++)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-
-					if (b == '3' && c == '5' && d == '9')
+					for (d = '0'; d <= '9'; d++)
 					{
-						continue;
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
 						_putchar('\n');
 					}
 				}
