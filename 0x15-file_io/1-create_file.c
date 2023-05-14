da0f2d8b4a4ea0e63ac45ddffbc6079cd9bc008c
ochiengsulwe <kodongochieng@gmail.com>
2023-05-14 05:26:59 -0700

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ * @filename: file to create.
+ * @text_content: file content to be written to filename.
+ * Description: filename must have required permissions->rw-------
+ *	if the file already exists, permissions ain't to be changed.
+ * If the file exists, truncate it.
+ * Return: 1 on success, or
+ *	-1 on failure,
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (0);
+	if (!(write(fd, text_content, strlen(text_content))))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
