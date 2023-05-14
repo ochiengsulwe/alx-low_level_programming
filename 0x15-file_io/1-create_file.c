e197c11f328da7422b3e42c1ba9a63e0bc96e561
ochiengsulwe <kodongochieng@gmail.com>
2023-05-14 05:48:34 -0700

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
+	if (!(filename))
+		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
