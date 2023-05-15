7d333cbc8989542ccfe188c8a9f3fc3548ab3e79
ochiengsulwe <kodongochieng@gmail.com>
2023-05-15 11:02:12 -0700

    Refactor: changed return value of open() erro to -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	if (!text_content)
 	{
 		close(fd);
