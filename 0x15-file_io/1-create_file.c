d9334246e444794718339de6e39ed8c2b41ce244
ochiengsulwe <kodongochieng@gmail.com>
2023-05-15 07:51:52 -0700

    added: write() exceptions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	mode_t permissions = S_IRUSR | S_IWUSR;
 
 	if (!(filename))
-		return (-1);
-	if (chmod(filename, permissions) == -1)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
-	if (!(write(fd, text_content, strlen(text_content))))
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	if (!(write(fd, text_content, strlen(text_content))) ||
+			(write(fd, text_content, strlen(text_content) != strlen(text_content))))
 	{
 		close(fd);
 		return (-1);
