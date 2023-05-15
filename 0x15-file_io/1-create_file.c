b7f495de4ddc1d74ea8c2fbef23a44dac63b587e
ochiengsulwe <kodongochieng@gmail.com>
2023-05-15 08:07:24 -0700

    added: permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	mode_t permissions = S_IRUSR | S_IWUSR;
 
 	if (!(filename))
 		return (-1);
+	if (chmod(filename, permissions) == -1)
+		return (1);
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
