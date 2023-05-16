75ec02f91696814498a9c32137aff3a6f4429e6a
ochiengsulwe <kodongochieng@gmail.com>
2023-05-16 06:58:54 -0700

    cleanup arounf conditional statement

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int count = 0;
 	unsigned long int flip = 0;
 
-	if ( n == m)
+	if (n == m)
 		return (count);
 	flip = m ^ n;
 	while (flip)
