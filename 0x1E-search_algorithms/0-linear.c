f0f8838335bb47719e52761d3124a7a637a69556
ochiengsulwe <kodongochieng@gmail.com>
2024-03-07 10:26:00 +0300

    making betty happy

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @size: size of the array (number of elements)
  * @value: value to searched for
  *
- * Return: 1st index where the value is located or -1 if not found or array is null
+ * Return: 1st index where the value is or -1 if not found or array is null
  */
 int linear_search(int *array, size_t size, int value)
 {
